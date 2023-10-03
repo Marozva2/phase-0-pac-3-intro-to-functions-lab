ef28c677775fa3ac82742f959f6084ffd982925e
Marozva2 <117761671+Marozva2@users.noreply.github.com>
2023-10-03 14:58:58 +0300

    Updated index.js

--- index.js
+++ index.js
@@ -24,7 +24,6 @@ function logWhisper(strings){
 function sayHiToHeadphonedRoommate(string){
     if (string === string.toLowerCase()){
       return("I can't hear you!");
-    
     }
     else if(string === string.toUpperCase()){
       return("YES INDEED!")
